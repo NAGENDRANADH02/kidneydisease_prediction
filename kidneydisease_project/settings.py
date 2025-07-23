5a666da553589e99a3ec451ff59c928c9f1034d8
Nagendranadh <167599852+NAGENDRANADH02@users.noreply.github.com>
2025-07-24 00:02:24 +0530

    settings.py

--- kidneydisease_project/settings.py
+++ kidneydisease_project/settings.py
@@ -124,8 +124,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 
-# Render fix for collectstatic EOF error
+# Render-specific settings
 if os.environ.get('RENDER'):
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'predictor/static'),
+]
 
