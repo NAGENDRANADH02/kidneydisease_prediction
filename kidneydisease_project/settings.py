84a2e3782d51590abb86d6a78b7938195b359251
Nagendranadh <167599852+NAGENDRANADH02@users.noreply.github.com>
2025-08-27 21:04:34 +0530

    Update settings.py

--- kidneydisease_project/settings.py
+++ kidneydisease_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-qb*0kun_@80dv-+)p90s@+w&$qorhw)4@&f*6@xft&56-8!gw+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('RENDER') is None  # Set DEBUG=False on Render
 
-ALLOWED_HOSTS = ['kidneydisease-prediction.vercel.app', '.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
